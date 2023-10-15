52cee4182ecfd5c41e55452ff2a99b15b637ce41
hyohyo814 <sojaisam@gmail.com>
2023-10-15 15:53:13 -0700

    Complete day3part1

--- go/2022_day3/main.go
+++ go/2022_day3/main.go
@@ -6,11 +6,14 @@ import (
 	"strings"
 )
 
-func main()  {
+func main() {
 	/*
 		items - 1 of 2 compartments
 		A given rucksack always has the same number of items in each of its two comps
 	*/
+
+	result := 0
+	priority := priorityCreate()
 
 	input, err := readInput()
 	if err != nil {
@@ -18,18 +21,39 @@ func main()  {
 		return
 	}
 
-	for _, v := range input {
+	for i, v := range input {
 		strarr := strings.Split(v, "")
-		fmt.Println(strarr)
-		for i := range strarr {
-			tmpleft := strarr[:i+1]	
-			tmpright := strarr[i+1:]	
-			fmt.Println("lhs: ", tmpleft)
-			fmt.Println("rhs: ", tmpright)
-			
-			// check for matches using map between two slices
+		left := strarr[:len(strarr)/2]
+		right := strarr[len(strarr)/2:]
+
+		leftguide := make(map[string]int)
+		rightguide := make(map[string]int)
+
+		var target string
+		for i := range left {
+			if _, ok := leftguide[left[i]]; !ok {
+				leftguide[left[i]] = 1
+			}
+			if _, ok := rightguide[right[i]]; !ok {
+				rightguide[right[i]] = 1
+			}
+
+			if _, ok := leftguide[right[i]]; ok {
+				target = right[i]
+				continue
+			}
+			if _, ok := rightguide[left[i]]; ok {
+				target = left[i]
+				continue
+			}
+		}
+		fmt.Printf("The type found in both compartments for rucksack %v is: %v\n", i, target)
+		lookup := []rune(target)
+		if mm, ok := priority[lookup[0]]; ok {
+			result += mm
 		}
 	}
+	fmt.Printf("The sum of priorities: %v\n", result)
 }
 
 // Function to read and format input data from text file
@@ -48,4 +72,20 @@ func readInput() ([]string, error) {
 	}
 
 	return lines, nil
+}
+
+func priorityCreate() map[rune]int {
+	alphabetMap := make(map[rune]int)
+
+	// Populate the map with lowercase letters (a to z)
+	for i, letter := 'a', 1; i <= 'z'; i, letter = i+1, letter+1 {
+		alphabetMap[i] = letter
+	}
+
+	// Populate the map with uppercase letters (A to Z)
+	for i, letter := 'A', 27; i <= 'Z'; i, letter = i+1, letter+1 {
+		alphabetMap[i] = letter
+	}
+
+	return alphabetMap
 }
